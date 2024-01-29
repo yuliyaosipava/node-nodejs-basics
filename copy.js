4f2d661de4469e12a215f19887b18b095fed2068
Yuliya Osipava <74550722+yuliyaosipava@users.noreply.github.com>
2024-01-29 22:30:20 +0300

    feat: add new features

--- copy.js
+++ copy.js
@@ -1,1 +1,7 @@
-copy.js
+const fs = require('fs');
+
+fs.cp('c:/Users/37529/nod', 'c:/Users/37529/copy_nod', { recursive: true }, (err) => {
+  if (err) {
+    console.error(err);
+  }
+});
--- create.js
+++ create.js
@@ -1,1 +1,10 @@
-create.js
+const fs = require('fs');
+
+fs.writeFile('fresh.txt', 'I am fresh and young', function (error) {
+    if (error) throw error; // если возникла ошибка
+    console.log(
+        'Асинхронная запись файла завершена. Содержимое файла:'
+    );
+    let data = fs.readFileSync('fresh.txt', 'utf8');
+    console.log(data); // выводим считанные данные
+});
